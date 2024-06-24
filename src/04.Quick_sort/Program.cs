162827d2bd3da9bae4138ed972ca06f8232115e3
Mykola Nesterenko <m.nesterenko@levi9.com>
2024-06-24 08:56:02 +0300

    add quick sort

--- src/04.Quick_sort/Program.cs
+++ src/04.Quick_sort/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Quick sort!");
+
+var array = new List<int>{ 7, 4, 3, 6, 10, 1, 2, 8, 5, 9 };
+var sorted = QuickSort(array);
+
+Console.WriteLine(string.Join(", ", sorted));
+
+List<int> QuickSort(List<int> array)
+{
+    if (array.Count < 2)
+    {
+        return array;
+    }
+
+    var pivot = array[0];
+    var less = array.Where(x => x < pivot).ToList();
+    var greater = array.Where(x => x > pivot).ToList();
+
+    return [..QuickSort(less), pivot, ..QuickSort(greater)];
+}
