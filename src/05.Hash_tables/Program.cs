4405a0c3a35e3dffc9b1f590891ce254269c4bf4
Mykola Nesterenko <m.nesterenko@levi9.com>
2024-06-30 10:31:09 +0300

    add hash tables

---
+++ src/05.Hash_tables/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hash tables");
+
+var voted = new Dictionary<string, bool>();
+CheckVote("mike");
+CheckVote("tom");
+CheckVote("mike");
+
+
+void CheckVote(string name)
+{
+    if (voted.ContainsKey(name))
+    {
+        Console.WriteLine("kick them out!");
+    }
+    else
+    {
+        voted[name] = true;
+        Console.WriteLine("let them vote {0}", name);
+    }
+}
